afd06ad0316f9e4c8fe0fa200c6724ea199926b1
mariadelcg7 <mariadelcg7@gmail.com>
2023-12-03 20:14:56 -0300

    documentación y ajuste en el código

--- home/index.php
+++ home/index.php
@@ -1,246 +1,157 @@
-    
-<?php include '../includes/head.php';?>
+    <?php include '../includes/head.php'; ?>
+
+    </head>
+
+    </head>
+
+    <body>
 
 
-</head>
+      <!-- menu horizontal -->
 
-</head>
+      <div class="menuhorizontal">
+        <ul>
+          <li class="dropdown">
+            <a href="#" class="dropbtn">Prematuros</a>
+            <div class="dropdown-content">
+              <a href="http://bebulindo.com.ar/lista/ConjuntosBa">conjuntos con batita</a>
+              <a href="http://bebulindo.com.ar/lista/ConjuntosBo">conjuntos con body</a>
 
-
-<body>
-
-
-
-
-<div class="menuhorizontal">
-    <ul>
-       <li class="dropdown">
-        <a href="#" class="dropbtn">Prematuros</a>
-        <div class="dropdown-content">
-          <a href="http://bebulindo.com.ar/lista/ConjuntosBa">conjuntos con batita</a>
-          <a href="http://bebulindo.com.ar/lista/ConjuntosBo">conjuntos con body</a>
-       
-        </div>
-      </li>
-
-
-	<li class="dropdown">
-        <a href="#" class="dropbtn">RN</a>
-        <div class="dropdown-content">
-          <a href="http://bebulindo.com.ar/lista/ConjuntosRN">conjuntos</a>
-          <a href="http://bebulindo.com.ar/lista/BodysRN">bodys ml</a>
-        </div>
-      </li>
-
-
-
-      <li class="dropdown">
-        <a href="#" class="dropbtn">Bebés</a>
-        <div class="dropdown-content">
-          <a href="http://bebulindo.com.ar/lista/BodysB">bodys</a>
-          <a href="http://bebulindo.com.ar/lista/MusculosasB">musculosas</a>
-          <a href="http://bebulindo.com.ar/lista/ShortsB">shorts</a>
-        </div>
-      </li>
-
-
-      <li class="dropdown">
-        <a href="#" class="dropbtn">Niños</a>
-        <div class="dropdown-content">
-          <a href="http://bebulindo.com.ar/lista/RemerasN">remeras</a>
-          <a href="http://bebulindo.com.ar/lista/MusculosasN">musculosas</a>
-          <a href="http://bebulindo.com.ar/lista/BermudasN">bermudas</a>
-        </div>
-      </li>
-
-
-    </ul>
-  </div>
-<br>
-<!-- Slider main container -->
-<div class="swiper">
-  <!-- Additional required wrapper -->
-  <div class="swiper-wrapper">
-    <!-- Slides -->
-    <div class="swiper-slide"><img src="../images/pasador/imagen3.webp"/></div>
-    <div class="swiper-slide"><img src="../images/pasador/imagen2.webp"/></div>
-    <div class="swiper-slide"><img src="../images/pasador/imagen1.webp"/></div>    
-  </div>
-  <!-- If we need pagination -->
-  <div class="swiper-pagination"></div>
-
-  <!-- If we need navigation buttons -->
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
-
-  <!-- If we need scrollbar -->
-  <div class="swiper-scrollbar"></div>
-</div>
-<script>
-    
-/*const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});*/
-
-    var swiper = new Swiper('.swiper', {
-  slidesPerView: 'auto',
-  spaceBetween: 20,
-  centeredSlides: false,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    loop: true,
-  },
-    // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  
-});
-
-
-</script>
-
-<style>
-    
-    .swiper {
-  width: 100%;
-  
-  
-}
-
-.swiper-wrapper{
-    width: 100%;
-    max-width:640px;
-}
-
-
-.swiper-button-next, .swiper-button-prev {
-  color: #88A765; /* Cambia esto al color que desees */
-}
-
-/* Estilos personalizados para la paginación */
-.swiper-pagination-bullet {
-  background-color:#88A765; /* Cambia esto al color que desees */
-}
-
-.swiper-pagination-bullet-active {
-  background-color: #789656; /* Cambia esto al color que desees */
-}
-
-
-</style>
-
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-<div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
-
-
-
-
-
-
-
-
-
-
-<!-- Footer  - redes sociales e informacion contacto -->
-
-    <footer>
-        <div class="footer-content">
-            <!-- Columna de redes sociales -->
-            <div class="column">
-                <h3>Redes Sociales</h3>
-
-               
-
-                <a href="https://www.facebook.com/bebulindo/" class="reds"><img width="50%" style="max-width: 35px" 
-              src="../../tienda/images/logof.png"/></a>
-            <a class="tifbi"  href="https://www.facebook.com/bebulindo/"><b></a>
-            
-                <a href="https://www.instagram.com/unbebulindo/" class="reds"><img width="50%" style="max-width: 35px" 
-              src="../../tienda/images/logoi.png"/></a>
-              <a class="tifbi"  href="https://www.instagram.com/unbebulindo/"><b></a>  
             </div>
-            
-            <!-- Columna de información de contacto -->
-            <div class="column">
-    <h3>Contacto</h3>
-    <p><i class="fas fa-phone"></i>  <a href="https://wa.me/543436209824" target="_blank">+54 343 6209824</a></p>
+          </li>
 
-    <p><i class="fas fa-envelope"></i> <a href="mailto:unbebulindo@gmail.com">unbebulindo@gmail.com</a></p>
 
-    
-</div>
+          <li class="dropdown">
+            <a href="#" class="dropbtn">RN</a>
+            <div class="dropdown-content">
+              <a href="http://bebulindo.com.ar/lista/ConjuntosRN">conjuntos</a>
+              <a href="http://bebulindo.com.ar/lista/BodysRN">bodys ml</a>
+            </div>
+          </li>
+
+
+
+          <li class="dropdown">
+            <a href="#" class="dropbtn">Bebés</a>
+            <div class="dropdown-content">
+              <a href="http://bebulindo.com.ar/lista/BodysB">bodys</a>
+              <a href="http://bebulindo.com.ar/lista/MusculosasB">musculosas</a>
+              <a href="http://bebulindo.com.ar/lista/ShortsB">shorts</a>
+            </div>
+          </li>
+
+
+          <li class="dropdown">
+            <a href="#" class="dropbtn">Niños</a>
+            <div class="dropdown-content">
+              <a href="http://bebulindo.com.ar/lista/RemerasN">remeras</a>
+              <a href="http://bebulindo.com.ar/lista/MusculosasN">musculosas</a>
+              <a href="http://bebulindo.com.ar/lista/BermudasN">bermudas</a>
+            </div>
+          </li>
+
+
+        </ul>
+      </div>
+
+
+      <br>
+      <!-- Slider main container -->
+      <div class="swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+          <!-- Slides -->
+          <div class="swiper-slide"><img src="../images/pasador/imagen3.webp" /></div>
+          <div class="swiper-slide"><img src="../images/pasador/imagen2.webp" /></div>
+          <div class="swiper-slide"><img src="../images/pasador/imagen1.webp" /></div>
+        </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
+
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+        <!-- If we need scrollbar -->
+        <div class="swiper-scrollbar"></div>
+      </div>
+      <script>
+
+
+        var swiper = new Swiper('.swiper', {
+          slidesPerView: 'auto',
+          spaceBetween: 20,
+          centeredSlides: false,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            loop: true,
+          },
+          // Navigation arrows
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+
+        });
+      </script>
+
+      
+
+      <div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
+      <div style="width: 300px;height: 300px; border: 1px solid #000; margin: 50px;"></div>
+
+
+
+
+
+
+
+      <!-- Footer  para  redes sociales e informacion contacto -->
+
+      <footer>
+        <div class="footer-content">
+          <!-- Columna de redes sociales -->
+          <div class="column">
+            <h3>Redes Sociales</h3>
+
+            <a href="https://www.facebook.com/bebulindo/" class="reds"><img width="50%" style="max-width: 35px" src="../../tienda/images/logof.png" /></a>
+            <a class="tifbi" href="https://www.facebook.com/bebulindo/"><b></a>
+
+            <a href="https://www.instagram.com/unbebulindo/" class="reds"><img width="50%" style="max-width: 35px" src="../../tienda/images/logoi.png" /></a>
+            <a class="tifbi" href="https://www.instagram.com/unbebulindo/"><b></a>
+          </div>
+
+          <!-- Columna de información de contacto -->
+          <div class="column">
+            <h3>Contacto</h3>
+            <p><i class="fas fa-phone"></i> <a href="https://wa.me/543436209824" target="_blank">+54 343 6209824</a></p>
+
+            <p><i class="fas fa-envelope"></i> <a href="mailto:unbebulindo@gmail.com">unbebulindo@gmail.com</a></p>
+
+
+          </div>
 
         </div>
-        
 
-         
         <!-- Derechos Reservados -->
         <div class="rights">
-            <p>&copy; 2023 Bebulindo. Todos los derechos reservados.</p>
+          <p>&copy; 2023 Bebulindo. Todos los derechos reservados.</p>
         </div>
 
-
+      <!-- boton wsp siempre visible parte inferior derecha  -->
 
         <div class="whatsapp-icon">
-    <a href="https://api.whatsapp.com/send?phone=5493436209824&text=Información">
-        <img class="custom-icon" src="../../tienda/images/wsp.png" alt="Icono wsp">
-    </a>
-</div>
+          <a href="https://api.whatsapp.com/send?phone=5493436209824&text=Información">
+            <img class="custom-icon" src="../../tienda/images/wsp.png" alt="Icono wsp">
+          </a>
+        </div>
+
+      </footer>
+
+
+    </body>
 
 
 
-
-
-
-
-
-
-        
-    </footer>
-
-
-</body>
-
-
-
-</html>
+    </html>
--- prod/indexlista.php
+++ prod/indexlista.php
@@ -1,182 +1,181 @@
-<?php include '../includes/head.php';?>
-<?php include '../includes/conexion.php';?>
+<?php include '../includes/head.php'; ?>
+<?php include '../includes/conexion.php'; ?>
 <?php
-$busqueda= " AND venta = 0";
-$sinbus=1;
-$vueltas=0;
+$busqueda = " AND venta = 0";    // productos en stock
+$sinbus = 1;    // busqueda
+$vueltas = 0;
 
 
+// titulo
+
+echo "<h3> " . $titulo . " </h3><br>";
 
 
+// lista de los productos en el menu
 
 
-if(isset($_GET["str"]) and $_GET["str"] === "RemerasN" )
-  {$busqueda.=" AND or_arr = '14' AND categ1 = 'N' AND tbase >=4 AND nro_pren = '1' 
-AND LOCATE('9',nro_temp) = 0 
+// lista para prematuros
+
+if (isset($_GET["str"]) and $_GET["str"] === "ConjuntosBa") {
+  $busqueda .= " AND or_arr = '16' AND categ1 = 'B' AND ordEdad < '-1'
+  AND nro_pren > 1  AND LOCATE('9',nro_temp) = 0 
+  order by   nro ";
+  $sinbus = 0;
+  $titulo = "CONJUNTOS PARA PREMATUROS";
+}
+
+
+if (isset($_GET["str"]) and $_GET["str"] === "ConjuntosBo") {
+  $busqueda .= " AND or_arr = '11' AND categ1 = 'B' AND ordEdad < '-1'
+  AND nro_pren > 1  AND LOCATE('9',nro_temp) = 0 
+  order by   nro desc";
+  $sinbus = 0;
+  $titulo = "CONJUNTOS PARA PREMATUROS";
+}
+
+
+// lista para recien nacido
+
+
+if (isset($_GET["str"]) and $_GET["str"] === "ConjuntosRN") {
+  $busqueda .= " AND or_arr = '11' AND categ1 = 'B' AND ordEdad = -1
+AND nro_pren > 1  AND LOCATE('9',nro_temp) = 0 
 order by   nro ";
- $sinbus=0;  $titulo="REMERAS PARA NIÑOS";}
+  $sinbus = 0;
+  $titulo = "CONJUNTOS PARA RECIEN NACIDO";
+}
 
 
-if(isset($_GET["str"]) and $_GET["str"] === "MusculosasN" )
-  {$busqueda.=" AND or_arr = '15' AND categ1 = 'N' AND tbase >=4  AND nro_pren = '1' 
-AND LOCATE('9',nro_temp) = 0 
-order by   nro ";
- $sinbus=0;  $titulo="MUSCULOSAS PARA NIÑOS";}
-
-
-if(isset($_GET["str"]) and $_GET["str"] === "BermudasN" )
-  {$busqueda.=" AND or_aba = '27' AND categ1 = 'N' AND tbase >=4  AND nro_pren = '1' 
-AND LOCATE('9',nro_temp) = 0 
+if (isset($_GET["str"]) and $_GET["str"] === "BodysRN") {
+  $busqueda .= " AND or_arr = '11' AND categ1 = 'B' AND ordEdad = -1 
+AND nro_pren = 1  AND LOCATE('9',nro_temp) = 0 
 order by   nro desc";
- $sinbus=0;  $titulo="BERMUDAS PARA NIÑOS";}
+  $sinbus = 0;
+  $titulo = "BODYS MANGA LARGAS RN";
+}
+
+
+//lista bebés
+
+
+if (isset($_GET["str"]) and $_GET["str"] === "BodysB") {
+  $busqueda .= " AND or_arr = '12' AND categ1 = 'B' 
+  AND tbase >0   AND nro_pren = '1'   
+  AND LOCATE('9',nro_temp) = 0 
+  order by  tbase desc, nro desc ";
+  $sinbus = 0;
+  $titulo = "BODYS PARA BEBES";
+}
+
+
+if (isset($_GET["str"]) and $_GET["str"] === "MusculosasB") {
+  $busqueda .= " AND or_arr = '15' AND categ1 = 'B' 
+  AND tbase >0 AND nro_pren = '1'   
+  AND LOCATE('9',nro_temp) = 0 
+  order by   nro ";
+  $sinbus = 0;
+  $titulo = "MUSCULOSAS PARA BEBES";
+}
+
+
+if (isset($_GET["str"]) and $_GET["str"] === "ShortsB") {
+  $busqueda .= " AND or_aba = '28' AND categ1 = 'B' 
+  AND tbase >0 AND nro_pren = '1'   
+  AND LOCATE('9',nro_temp) = 0 
+  order by   nro desc";
+  $sinbus = 0;
+  $titulo = "SHORTS PARA BEBES";
+}
+
+
+
+// lista niños 
+
+if (isset($_GET["str"]) and $_GET["str"] === "RemerasN") {
+  $busqueda .= " AND or_arr = '14' AND categ1 = 'N' 
+  AND tbase >=4 AND nro_pren = '1' 
+  AND LOCATE('9',nro_temp) = 0 
+  order by   nro ";
+  $sinbus = 0;
+  $titulo = "REMERAS PARA NIÑOS";
+}
+
+
+if (isset($_GET["str"]) and $_GET["str"] === "MusculosasN") {
+  $busqueda .= " AND or_arr = '15' AND categ1 = 'N' 
+  AND tbase >=4  AND nro_pren = '1' 
+  AND LOCATE('9',nro_temp) = 0 
+  order by   nro ";
+  $sinbus = 0;
+  $titulo = "MUSCULOSAS PARA NIÑOS";
+}
+
+
+if (isset($_GET["str"]) and $_GET["str"] === "BermudasN") {
+  $busqueda .= " AND or_aba = '27' AND categ1 = 'N' 
+  AND tbase >=4  AND nro_pren = '1' 
+  AND LOCATE('9',nro_temp) = 0 
+  order by   nro desc";
+  $sinbus = 0;
+  $titulo = "BERMUDAS PARA NIÑOS";
+}
 
 
 
 
 
-
-
-if(isset($_GET["str"]) and $_GET["str"] === "BodysB" )
-  {$busqueda.=" AND or_arr = '12' AND categ1 = 'B' AND tbase >0  AND codigo <> 4003 AND nro_pren = '1'   
-AND LOCATE('9',nro_temp) = 0 
-order by  tbase desc, nro desc ";
- $sinbus=0;  $titulo="BODYS PARA BEBES";}
-
-
-
-if(isset($_GET["str"]) and $_GET["str"] === "MusculosasB" )
-  {$busqueda.=" AND or_arr = '15' AND categ1 = 'B' AND tbase >0 AND nro_pren = '1'   
-AND LOCATE('9',nro_temp) = 0 
-order by   nro ";
- $sinbus=0;  $titulo="MUSCULOSAS PARA BEBES";}
-
-
-
-if(isset($_GET["str"]) and $_GET["str"] === "ShortsB" )
-  {$busqueda.=" AND or_aba = '28' AND categ1 = 'B' AND tbase >0 AND nro_pren = '1'   
-AND LOCATE('9',nro_temp) = 0 
-order by   nro desc";
- $sinbus=0;  $titulo="SHORTS PARA BEBES";}
-
-
-
-
-
-
-
-
-
-if(isset($_GET["str"]) and $_GET["str"] === "ConjuntosRN" )
-  {$busqueda.=" AND or_arr = '11' AND categ1 = 'B' AND tbase = 0
-   AND nro_pren > 1  AND LOCATE('9',nro_temp) = 0 
-order by   nro desc";
- $sinbus=0;  $titulo="CONJUNTOS PARA RECIEN NACIDO";}
-
-
-
-if(isset($_GET["str"]) and $_GET["str"] === "BodysRN" )
-  {$busqueda.=" AND or_arr = '11' AND categ1 = 'B' AND tbase = 0 AND codigo <> 15
-   AND nro_pren = 1  AND LOCATE('9',nro_temp) = 0 
-order by   nro desc";
- $sinbus=0;  $titulo="BODYS MANGA LARGAS RN";}
-
-
-
-
-
-if(isset($_GET["str"]) and $_GET["str"] === "ConjuntosBa" )
-  {$busqueda.=" AND or_arr = '16' AND categ1 = 'B' AND ordEdad < '-1'
-   AND nro_pren > 1  AND LOCATE('9',nro_temp) = 0 
-order by   nro ";
- $sinbus=0;  $titulo="CONJUNTOS PARA PREMATUROS";}
-
-
-
-
-if(isset($_GET["str"]) and $_GET["str"] === "ConjuntosBo" )
-  {$busqueda.=" AND or_arr = '11' AND categ1 = 'B' AND ordEdad < '-1'
-   AND nro_pren > 1  AND LOCATE('9',nro_temp) = 0 
-order by   nro desc";
- $sinbus=0;  $titulo="CONJUNTOS PARA PREMATUROS";}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-echo "<h3> ".$titulo." </h3><br>";
-
-
-function codArt($msg) {   
-    $length = 6;
-    $string = substr(str_repeat(0, $length).$msg, - $length); //completo con ceros hasta alcanzar 6
-    $sumaimpar=($string[0]+$string[2]+$string[4])*3; 
-    $sumapar=$string[1]+$string[3]+$string[5];  
-    $resto=($sumapar+$sumaimpar)%10;  //la unidades de sumar los impares por 3, mas los pares
-    $string.=$resto;  //eso se agrega como caracter de verificacion
-    $string = substr(str_repeat(0, $length).dechex($string), - $length);  //convierto en hexa
-    $coNum= array ( '102','99','116','113','104','106','97','49','108','105','112','73','98','100','101','111'); 
-$salida="";
-for($i=0;$i<strlen($string);$i++){$salida.=chr($coNum[hexdec($string[$i])]);} //codifico las cifras hexa por otras del vector llave
-  $salida2="FFFFFF";
-  $salida2[0]=$salida[2];
-  $salida2[1]=$salida[4];
-  $salida2[2]=$salida[0];
-  $salida2[3]=$salida[5];
-  $salida2[4]=$salida[1];
-  $salida2[5]=$salida[3];    //cambié el orden de las cifras
+//funcion codigo-articulo
+function codArt($msg)
+{
+  $length = 6;
+  $string = substr(str_repeat(0, $length) . $msg, -$length); //completo con ceros hasta alcanzar 6
+  $sumaimpar = ($string[0] + $string[2] + $string[4]) * 3;
+  $sumapar = $string[1] + $string[3] + $string[5];
+  $resto = ($sumapar + $sumaimpar) % 10;  //la unidades de sumar los impares por 3, mas los pares
+  $string .= $resto;  //eso se agrega como caracter de verificacion
+  $string = substr(str_repeat(0, $length) . dechex($string), -$length);  //convierto en hexa
+  $coNum = array('102', '99', '116', '113', '104', '106', '97', '49', '108', '105', '112', '73', '98', '100', '101', '111');
+  $salida = "";
+  for ($i = 0; $i < strlen($string); $i++) {
+    $salida .= chr($coNum[hexdec($string[$i])]);
+  } //codifico las cifras hexa por otras del vector llave
+  $salida2 = "FFFFFF";
+  $salida2[0] = $salida[2];
+  $salida2[1] = $salida[4];
+  $salida2[2] = $salida[0];
+  $salida2[3] = $salida[5];
+  $salida2[4] = $salida[1];
+  $salida2[5] = $salida[3];    //cambié el orden de las cifras
   return $salida2;
- 
 }
 
 
 
+// consulta BD productos 
+
+if ($sinbus === 0) {
+  $consulta_prod = "SELECT * FROM tienda WHERE id_prod > 0" . $busqueda;
+  $resultado_prod = mysqli_query($conexion, $consulta_prod) or die("<div class='nadaquever'>Algo ha ido mal en la consulta a la base de datos</div>");
 
 
-if($sinbus===0){
-$consulta_prod ="SELECT * FROM stock WHERE id_prod > 0".$busqueda;
-$resultado_prod = mysqli_query( $conexion, $consulta_prod ) or die ( "<div class='nadaquever'>Algo ha ido mal en la consulta a la base de datos</div>");
- 
+  // muestra resultados   
 
+  while ($producto = mysqli_fetch_array($resultado_prod)) {
 
-while ($producto= mysqli_fetch_array( $resultado_prod))
-  {
-
-  $palabra="Comprar"; 
-  include '../includes/whileprod.php';
+    $palabra = "Comprar";
+    include '../includes/whileprod.php';
   }
-
 }
 
+// si no hay nada para mostrar 
 
-
-if($sinbus===1 or $vueltas===0 ) {
-  echo "<div class='nadaquever'>No hay productos que mostrar.</div>";}
-
-
-
-
-
- ?>
-
-
+if ($sinbus === 1 or $vueltas === 0) {
+  echo "<div class='nadaquever'>No hay productos que mostrar.</div>";
+}
+?>
 
 
 
 </body>
-</html>
 
+</html>
