46f029e5952a12b1d9a3bd095741e60c43246f4d
MariaDelCGodoy <102637778+MariaDelCGodoy@users.noreply.github.com>
2023-12-06 16:43:30 -0300

    includes footer

---
+++ includes/footer.php
@@ -0,0 +1,45 @@
+
+
+      <!-- Footer  para  redes sociales e informacion contacto -->
+
+      <footer>
+        <div class="footer-content">
+          <!-- Columna de redes sociales -->
+          <div class="column">
+            <h3>Redes Sociales</h3>
+
+
+            <a href="https://www.facebook.com/bebulindo/" class="reds"><img width="50%" style="max-width: 35px" src="../../tienda/images/logof.png" /></a>
+            <a class="tifbi" href="https://www.facebook.com/bebulindo/"><b></a>
+
+            <a href="https://www.instagram.com/unbebulindo/" class="reds"><img width="50%" style="max-width: 35px" src="../../tienda/images/logoi.png" /></a>
+            <a class="tifbi" href="https://www.instagram.com/unbebulindo/"><b></a>
+          </div>
+
+          <!-- Columna de información de contacto -->
+          <div class="column">
+            <h3>Contacto</h3>
+
+            <p><i class="fas fa-phone"></i> <a href="https://wa.me/543436209824" target="_blank">+54 343 6209824</a></p>
+
+            <p><i class="fas fa-envelope"></i> <a href="mailto:unbebulindo@gmail.com">unbebulindo@gmail.com</a></p>
+
+
+          </div>
+
+        </div>
+
+        <!-- Derechos Reservados -->
+        <div class="rights">
+          <p>&copy; 2023 Bebulindo. Todos los derechos reservados.</p>
+        </div>
+
+      <!-- boton wsp siempre visible parte inferior derecha  -->
+
+        <div class="whatsapp-icon">
+          <a href="https://api.whatsapp.com/send?phone=5493436209824&text=Información">
+            <img class="custom-icon" src="../../tienda/images/wsp.png" alt="Icono wsp">
+          </a>
+        </div>
+
+      </footer>
